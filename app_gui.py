07ff64ffce865e2e9cec8629537b9603a954772b
Krzysztof Bera <krzysztof.bera9@gmail.com>
2021-05-10 23:09:01 +0200

    cleaning up a little bit

--- app_gui.py
+++ app_gui.py
@@ -22,15 +22,11 @@ class AppGui(tkinter.Tk):
 
         self.pytube_actions = PytubeActions(self)
 
-        self.link_label = None
         self.link_entry = None
-        self.button_info_frame = None
         self.button_info = None
-        self.title_label = None
         self.title_entry = None
         self.format_label = None
         self.format_combobox = None
-        self.button_download_frame = None
         self.button_download = None
         self.create_link_box()
         self.create_button_info()
@@ -51,10 +47,10 @@ class AppGui(tkinter.Tk):
         Initializing the entry box where user pastes his link
         """
 
-        self.link_label = tkinter.LabelFrame(self, text="Enter your link")
-        self.link_label.pack(pady=10)
+        link_label = tkinter.LabelFrame(self, text="Enter your link")
+        link_label.pack(pady=10)
 
-        self.link_entry = tkinter.Entry(self.link_label, font=("Helvetica", 24), width=50)
+        self.link_entry = tkinter.Entry(link_label, font=("Helvetica", 24), width=50)
         self.link_entry.pack(pady=20, padx=20)
 
     def create_button_info(self):
@@ -62,10 +58,10 @@ class AppGui(tkinter.Tk):
         Initializing the button, which clicking shows us information about the video
         """
 
-        self.button_info_frame = tkinter.Frame(self)
-        self.button_info_frame.pack(pady=5, padx=5)
+        button_info_frame = tkinter.Frame(self)
+        button_info_frame.pack(pady=5, padx=5)
 
-        self.button_info = tkinter.Button(self.button_info_frame, text="Show info",
+        self.button_info = tkinter.Button(button_info_frame, text="Show info",
                                           command=self.get_title_video)
         self.button_info.pack(pady=20, padx=20)
 
@@ -74,10 +70,10 @@ class AppGui(tkinter.Tk):
         Initializing the entry box where title of the video shows up
         """
 
-        self.title_label = tkinter.LabelFrame(self, text="Title of the video")
-        self.title_label.pack(pady=5)
+        title_label = tkinter.LabelFrame(self, text="Title of the video")
+        title_label.pack(pady=5)
 
-        self.title_entry = tkinter.Entry(self.title_label, font=("Helvetica", 24), width=50)
+        self.title_entry = tkinter.Entry(title_label, font=("Helvetica", 24), width=50)
         self.title_entry.pack(pady=10, padx=10)
 
     def create_format_combobox(self):
@@ -97,10 +93,10 @@ class AppGui(tkinter.Tk):
         Initializing the button which is responsible for downloading the video
         """
 
-        self.button_download_frame = tkinter.Frame(self)
-        self.button_download_frame.pack(pady=5, padx=5)
+        button_download_frame = tkinter.Frame(self)
+        button_download_frame.pack(pady=5, padx=5)
 
-        self.button_download = tkinter.Button(self.button_download_frame, text="Download video",
+        self.button_download = tkinter.Button(button_download_frame, text="Download video",
                                               command=self.download_video)
         self.button_download.pack(pady=5, padx=5)
 
@@ -123,6 +119,7 @@ class AppGui(tkinter.Tk):
         """
         This function invokes function for PytubeAction class
         """
+
         try:
             self.pytube_actions.download_video()
         except exceptions.FormatNotProvided:
