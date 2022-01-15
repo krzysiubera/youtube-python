1caa849a5d725536dce3f6ba05d1f4b25a7929dc
Krzysztof Bera <krzysztof.bera9@gmail.com>
2022-01-15 19:45:27 +0100

    add list of downloaded videos

--- application.py
+++ application.py
@@ -1,27 +1,30 @@
 import tkinter as tk
 import tkinter.filedialog
 import tkinter.messagebox
-from pytube_handler import download_video
-from typing import Tuple
+from typing import Tuple, List
 from configparser import ConfigParser
 from pathlib import Path
+import datetime
+from pytube_handler import PytubeHandler
 from exceptions import WrongLinkProvidedError, VideoUnavailableError
 
 
 class Application(tk.Tk):
     """ Class representing application """
 
-    initial_path_to_download = r"/Users/krzysiu/Desktop"
-
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.initialize_window()
         self.link_entry = self.create_link_box()
         self.file_path_entry, self.file_path_button = self.create_file_path_box()
         self.button_download = self.create_button_download()
         self.initial_path = self.get_initial_path()
+        self.video_listbox, self.clear_button = self.create_video_listbox()
 
-    def initialize_window(self):
+        self.downloaded_videos_info: List[str] = []
+        self.pytube_handler = PytubeHandler()
+
+    def initialize_window(self) -> None:
         self.title('Python Youtube')
         self.geometry('800x800')
 
@@ -35,13 +38,14 @@ class Application(tk.Tk):
         return link_entry
 
     def create_file_path_box(self) -> Tuple[tk.Entry, tk.Button]:
-        """ Entry for path to the file and button"""
+        """ Entry for path to the file and button for selecting the path"""
         file_path_label_frame = tk.LabelFrame(self, text='Path to save the video')
         file_path_label_frame.pack(pady=5)
 
         file_path_entry = tk.Entry(file_path_label_frame, font="Helvetica, 24", width=50)
         file_path_entry.pack(pady=5)
-        file_path_button = tk.Button(file_path_label_frame, text="Select file path", command=self.get_file_path)
+        file_path_button = tk.Button(file_path_label_frame, text="Select file path",
+                                     command=self.handle_select_file_path)
         file_path_button.pack(pady=5)
         return file_path_entry, file_path_button
 
@@ -50,34 +54,62 @@ class Application(tk.Tk):
         button_download_frame = tk.Frame(self)
         button_download_frame.pack(pady=5, padx=5)
 
-        button_download = tk.Button(button_download_frame, text="Download video", command=self.download_video)
+        button_download = tk.Button(button_download_frame, text="Download video", command=self.handle_download_video)
         button_download.pack(pady=5, padx=5)
         return button_download
 
-    def get_file_path(self):
+    def create_video_listbox(self) -> Tuple[tk.Listbox, tk.Button]:
+        """ List box for storing downloaded videos and button for clearing it """
+        video_textbox_frame = tk.Frame(self)
+        video_textbox_frame.pack(pady=5, padx=5)
+
+        video_textbox = tk.Listbox(video_textbox_frame, height=10, width=100)
+        video_textbox.pack(pady=20, padx=20)
+        clear_button = tk.Button(video_textbox_frame, text="Clear downloaded videos",
+                                 command=self.handle_clear_downloaded_videos)
+        clear_button.pack(pady=5, padx=5)
+        return video_textbox, clear_button
+
+    def handle_select_file_path(self) -> None:
         """ Callback when 'Select file path' button is clicked """
         self.file_path_entry.delete(0, tk.END)
         file_path = tk.filedialog.askdirectory(initialdir=self.initial_path, title='Select file path')
         self.file_path_entry.insert(0, file_path)
 
-    def download_video(self):
+    def handle_download_video(self) -> None:
         """ Callback when 'Download video' button is clicked """
         link_video = self.link_entry.get()
         file_path = self.file_path_entry.get()
         try:
-            download_video(link_video, file_path)
+            self.pytube_handler.download_video(link_video, file_path)
         except WrongLinkProvidedError:
             tk.messagebox.showerror("Error", "Wrong video link provided")
         except VideoUnavailableError:
             tk.messagebox.showerror("Error", "Video unavailable")
+        else:
+            self.update_list_downloaded_videos()
+
+    def handle_clear_downloaded_videos(self) -> None:
+        """ Callback when 'Clear downloaded videos' button is clicked """
+        self.downloaded_videos_info = []
+        self.video_listbox.delete(0, tk.END)
+
+    def update_list_downloaded_videos(self) -> None:
+        """ When the video is downloaded, the function handles putting it into list of downloaded videos """
+        title = self.pytube_handler.get_last_video_title()
+        author = self.pytube_handler.get_last_video_author()
+        msg = f"{datetime.datetime.now().strftime('%d/%m/%y, %H:%M:%S')}: {title} by {author}"
+        self.downloaded_videos_info.append(msg)
+        self.video_listbox.insert(tk.END, self.downloaded_videos_info[-1])
 
     @staticmethod
     def get_initial_path() -> Path:
+        """ Path to download files shown initially in file dialog """
         config = ConfigParser()
         config.read('config.ini')
         initial_path = config['paths']['initial_path']
         return Path(initial_path)
 
-    def run_application(self):
+    def run_application(self) -> None:
         """ Start event loop of the application """
         self.mainloop()
--- pytube_handler.py
+++ pytube_handler.py
@@ -3,15 +3,34 @@ import pytube.exceptions
 from exceptions import WrongLinkProvidedError, VideoUnavailableError
 
 
-def download_video(link_video, file_path):
-    """ Downloading video """
-    try:
-        youtube = pytube.YouTube(link_video)
-    except pytube.exceptions.RegexMatchError:
-        raise WrongLinkProvidedError
-    except pytube.exceptions.VideoUnavailable:
-        raise VideoUnavailableError
-    else:
-        youtube.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().\
-            download(file_path)
+class PytubeHandler:
+    """ Class for handling Pytube"""
+
+    def __init__(self) -> None:
+        self.last_video_title: str = ""
+        self.last_video_author: str = ""
+
+    def download_video(self, link_video: str, file_path: str):
+        """ Downloading video """
+        try:
+            youtube = pytube.YouTube(link_video)
+        except pytube.exceptions.RegexMatchError:
+            raise WrongLinkProvidedError
+        except pytube.exceptions.VideoUnavailable:
+            raise VideoUnavailableError
+        else:
+            youtube.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().\
+                download(file_path)
+            self.last_video_title = youtube.title
+            self.last_video_author = youtube.author
+
+    def get_last_video_title(self) -> str:
+        """ Get latest downloaded video name"""
+        return self.last_video_title
+
+    def get_last_video_author(self) -> str:
+        """ Get latest downloaded video author """
+        return self.last_video_author
+
+
 
